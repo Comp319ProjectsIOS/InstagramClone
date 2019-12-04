53c42f4466eb112bb6578801710f258a149020a8
melihozcelik <mozcelik17@ku.edu.tr>
2019-12-05 00:25:43 +0300

    Forget password logic added
    Forget password logic added

--- InstagramApp/InstagramApp/ViewControllers/ForgetPasswordViewController.swift
+++ InstagramApp/InstagramApp/ViewControllers/ForgetPasswordViewController.swift
@@ -7,16 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ForgetPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Forget Password"
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func resetPasswordTapped(_ sender: Any) {
+        userForgotPassword(emailTextField: emailTextField, vc: self)
+    }
+    
+    public func userForgotPassword(emailTextField: UITextField, vc: UIViewController) {
+        if let email = emailTextField.text {
+            Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+                if let error = error {
+                    PresentAlert(vc, alertStyle: .alert, title: "Error", message: error.localizedDescription, withCancel: false, withSegueIdentifier: SegueIdentifier())
+                }
+                PresentAlert(vc, alertStyle: .alert, title: "Sıfırlama maili gönderilmiştir!", message: "Lütfen \(email) mail adresini kontrol ediniz.", withCancel: false, withSegueIdentifier: SegueIdentifier())
+            }
+        }
+    }
+    
+    public func PresentAlert(_ viewController: UIViewController, alertStyle: UIAlertController.Style, title: String, message: String, withCancel cancel: Bool, withSegueIdentifier identifier: SegueIdentifier) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: alertStyle)
+        let defaultAction = UIAlertAction(title: "Tamam", style: .default) { (action) in
+            alertVC.dismiss(animated: true, completion: nil)
+            if identifier.exist {
+                viewController.performSegue(withIdentifier: identifier.identifier, sender: nil)
+            }
+        }
+        if cancel {
+            let cancelAction = UIAlertAction(title: "İptal", style: .cancel) { (action) in
+                alertVC.dismiss(animated: true, completion: nil)
+            }
+            alertVC.addAction(cancelAction)
+        }
+        alertVC.addAction(defaultAction)
+        viewController.present(alertVC, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
