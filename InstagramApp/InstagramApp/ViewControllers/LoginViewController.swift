285a6a6ec0a2b2343cdcc220554ccad3fda775b9
Basak Cortuk <bcortuk16@ku.edu.tr>
2019-12-05 00:23:58 +0300

    user login setup

--- InstagramApp/InstagramApp/ViewControllers/LoginViewController.swift
+++ InstagramApp/InstagramApp/ViewControllers/LoginViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginTapped(_ sender: Any) {
+        login()
+    }
+    
+    
+  // Bu kısım mvc violate mi ediyor???
+    func login() {
+        if let email = emailText.text {
+            if let password = passwordText.text {
+                Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                    if let error = error {
+                        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    } else {
+                        print("You are sign in.")
+                    }
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
